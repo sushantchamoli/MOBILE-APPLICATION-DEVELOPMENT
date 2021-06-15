98652c0337b45ceff6acf12d312f04e7fcbe8923
Ritik Gupta <55070019+Ritik25032000@users.noreply.github.com>
2021-06-15 11:08:33 +0530

    Add files via upload

---
+++ CSE-6C/Ritik_Gupta_1018561/Q3 - BoundService.java
@@ -0,0 +1,35 @@
+package com.ritikgupta.endsem;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BoundService extends Service {
+
+	private ArrayList<Integer> integers;
+	private final IBinder binder = new LocalBinder();
+
+	@Nullable
+	@Override
+	public IBinder onBind(Intent intent) {
+		integers = intent.getIntegerArrayListExtra("numbers");
+		return binder;
+	}
+
+	public ArrayList<Integer> sortNumbers() {
+		Collections.sort(integers);
+		return integers;
+	}
+
+	public class LocalBinder extends Binder {
+		BoundService getService() {
+			return BoundService.this;
+		}
+	}
+}
