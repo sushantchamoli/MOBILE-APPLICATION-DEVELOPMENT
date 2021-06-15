e97ca09205a36c19f3b1f2fa9e96070064e39037
Ayushi140320 <45115104+Ayushi140320@users.noreply.github.com>
2021-06-15 10:15:08 +0530

    Add files via upload

---
+++ CSE-6C/Ayushi_Sharma_1018470/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ayushi.ques1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText Name;
+    EditText Age;
+    EditText email;
+    EditText password;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Name=findViewById(R.id.Name);
+        email=findViewById(R.id.editTextTextEmailAddress);
+        Age=findViewById(R.id.editTextNumber);
+        password=findViewById(R.id.editTextTextPassword);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                i.putExtra("name",Name.getText().toString());
+                i.putExtra("age",Age.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ CSE-6C/Ayushi_Sharma_1018470/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.ayushi.ques1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView name;
+    TextView age;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        name = findViewById(R.id.textView5);
+        age = findViewById(R.id.textView6);
+        Intent i = getIntent();
+        name.setText(i.getStringExtra("name"));
+        age.setText(i.getStringExtra("age"));
+    }
+}
