64dc8d41eb673994eec6062ba170a0f3b0a51215
Devesh Kumar Srivastav <54620323+dk3101@users.noreply.github.com>
2021-06-15 10:25:11 +0530

    Create Q1Main1Activity.java

---
+++ Devesh_Kumar_Srivastav_1018477/Q1Main1Activity.java
@@ -0,0 +1,23 @@
+package com.example.q1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main1Activity extends AppCompatActivity
+{
+    TextView t1, t2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        t1 = findViewById(R.id.textView);
+        t2 = findViewById(R.id.textView5);
+        Intent intent = getIntent();
+        t1.setText(intent.getStringExtra("name"));
+        t2.setText(intent.getStringExtra("age"));
+    }
+}
