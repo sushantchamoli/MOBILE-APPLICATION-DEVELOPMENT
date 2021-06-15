85d002f6b6c377a92843e16bb0cc59feae2a9740
Nandini-05-goyal <79010963+Nandini-05-goyal@users.noreply.github.com>
2021-06-15 10:23:43 +0530

    Add files via upload

---
+++ CSE-6C/Nandini_Goyal_1018531/Ques2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pract2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+        // Array of strings...
+        ListView simpleList;
+        String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+    }
+}
