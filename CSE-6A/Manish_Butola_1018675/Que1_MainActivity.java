96f1bdaf5e83de680f6892d448d69e0fd68ee7f6
Manish-butola <79547520+Manish-butola@users.noreply.github.com>
2021-06-15 10:19:58 +0530

    Add files via upload

--- CSE-6A/Manish_Butola_1018675/Que1_MainActivity.java
+++ CSE-6A/Manish_Butola_1018675/Que1_MainActivity.java
@@ -1,26 +1,44 @@
-package com.example.loginform;
+package com.example.ras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-   private EditText name,age;
+    Button button;
+
+    EditText e1,e2,e3,e4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=findViewById(R.id.name);
-        age=findViewById(R.id.age);
+        button=findViewById(R.id.button);
 
-    }
-    public void sendToNext(View v){
-        Intent intent=new Intent(getApplicationContext(),NextActivity.class);
-        intent.putExtra("Name",name.getText().toString());
-        intent.putExtra("Age",age.getText().toString());
-        startActivity(intent);
+        e1=findViewById(R.id.ename);
+        e2=findViewById(R.id.email);
+        e3=findViewById(R.id.eage);
+        e4=findViewById(R.id.epass);
+        Intent i=new Intent( MainActivity.this,MainActivity2.class);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                i.putExtra("msg1",e1.getText().toString());
+                i.putExtra("msg2",e2.getText().toString());
+                i.putExtra("msg3",e3.getText().toString());
+                i.putExtra("msg4",e4.getText().toString());
+                startActivity(i);
+            }
+
+        });
+
     }
 }
