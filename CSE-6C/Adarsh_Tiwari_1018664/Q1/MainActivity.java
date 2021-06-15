e73e493abebe4b2784621bfbc7bf1239eed76ed8
Adarsh Tiwari <56510817+adarsht756@users.noreply.github.com>
2021-06-15 10:31:24 +0530

    Add files via upload

---
+++ CSE-6C/Adarsh_Tiwari_1018664/Q1/MainActivity.java
@@ -0,0 +1,53 @@
+package com.adarshtiwari.endsemquesone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ Name - Adarsh Tiwari
+* Section - C
+* Roll no - 1018664
+* Subject - Mobile Application Development
+ * */
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtName;
+    EditText edtAge;
+    EditText edtEmail;
+    EditText edtPassword;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtAge = findViewById(R.id.editAge);
+        edtEmail = findViewById(R.id.editEmail);
+        edtName = findViewById(R.id.editName);
+        edtPassword = findViewById(R.id.editPassword);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtName.getText().toString();
+                String age = edtAge.getText().toString();
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("name", name);
+                intent.putExtra("age", age);
+                intent.putExtra("password", password);
+                intent.putExtra("email", email);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
