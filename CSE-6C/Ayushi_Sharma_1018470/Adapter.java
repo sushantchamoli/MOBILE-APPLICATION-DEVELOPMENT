65a8a7d96b2670776c3fed8e06d4aa5e78176555
Ayushi140320 <45115104+Ayushi140320@users.noreply.github.com>
2021-06-15 10:31:00 +0530

    Add files via upload

---
+++ CSE-6C/Ayushi_Sharma_1018470/Adapter.java
@@ -0,0 +1,73 @@
+package com.ayushi.ques2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Map;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+    private Context aCtx;
+    private List<Map<String, String>> list;
+
+    public Adapter(Context aCtx, List<Map<String, String>> list){
+        this.aCtx=aCtx;
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(aCtx).inflate(R.layout.card_country, parent, false);
+        return new MyViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(list.get(position).get("name"));
+
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(aCtx, MainActivity2.class);
+            intent.putExtra("country",list.get(position).get("name"));
+            intent.putExtra("data",list.get(position).get("data"));
+            intent.putExtra("img",list.get(position).get("img"));
+
+            aCtx.startActivity(intent);
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list == null){
+            return 0;
+        }
+        return list.size();
+    }
+
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.text_country);
+
+        }
+    }
+
+
+}
+
+
+
--- CSE-6C/Ayushi_Sharma_1018470/MainActivity2.java
+++ CSE-6C/Ayushi_Sharma_1018470/MainActivity2.java
@@ -1,22 +1,33 @@
-package com.ayushi.ques1;
-
+package com.ayushi.ques2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity2 extends AppCompatActivity {
-    TextView name;
-    TextView age;
+    TextView countryText,countryData;
+    String stringCountry,stringCountryData,stringImg;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        name = findViewById(R.id.textView5);
-        age = findViewById(R.id.textView6);
-        Intent i = getIntent();
-        name.setText(i.getStringExtra("name"));
-        age.setText(i.getStringExtra("age"));
+        countryText =findViewById(R.id.text_country);
+        countryData = findViewById(R.id.text_country_data);
+        imageView = findViewById(R.id.img);
+        Intent intent = getIntent();
+        stringCountry = intent.getStringExtra("country");
+        stringCountryData = intent.getStringExtra("data");
+        stringImg = intent.getStringExtra("img");
+        System.out.println(stringImg);
+        countryText.setText(stringCountry);
+        countryData.setText(stringCountryData);
+        Glide.with(this).load(stringImg).into(imageView);
+
     }
 }
